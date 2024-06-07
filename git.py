668033bdb650495331e6b9978b2f84dc895c4f35
Victor Romeo <victoromeo52@gmail.com>
2024-06-07 16:09:52 +0300

    Change Year

--- git.py
+++ git.py
@@ -1,10 +1,13 @@
 import os, random
 
-for i in range(50):
-    d = str(i) + 'days ago'
-    rand = random.randrange(1, 12)
-    with open('test.txt', 'a') as file:
-        file.write(d + '\n')
-    os.system('git add test.txt')
-    os.system('git commit --date=" 2018-'+str(rand) + '-' + d + '" -m 1')
+current = 18
+for y in range(4):
+    for i in range(100):
+        d = str(i) + 'days ago'
+        rand = random.randrange(1, 12)
+        with open('test.txt', 'a') as file:
+            file.write(d + '\n')
+        os.system('git add test.txt')
+        os.system(f'git commit --date=" 20{current}-{str(rand)}-{d}" -m 1')
+    current += 1
 os.system('git push -u origin main -f')
